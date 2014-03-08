da3b5fb822b9607b89619175c6820833653395b0
Grant Marshall <grant.a.marshall@asu.edu>
2014-03-07 18:10:01 -0700

    Changed readme; added twittp script that will be new frontend for all twittp commands

---
+++ bin/twittp.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+def main():
+    """Parses arguments using argparse and executes corresponding code"""
+    command_parser = argparse.ArgumentParser(description='twittp -- Twitter Trend Prediction')
+    subparsers = command_parser.add_subparsers(title="commands")
+
+    build_model_parser = subparsers.add_parser('build-model', help='Build a '
+        'model for other actions in twittp')
+
+    build_model_parser.description = 'Build a model for other actions in twittp'
+
+    build_model_parser.add_argument('tweets', help='The JSON file containing '
+        'tweets from the Twitter API')
+    build_model_parser.add_argument('trends', help='The JSON file containing '
+        'trends from the Twitter API')
+    build_model_parser.add_argument('--stopword', help='An optional CSV file '
+        'containing words to ignore when constructing the model')
+
+    loo_test_parser = subparsers.add_parser('loo-test', help='Test the '
+        'performance of a model using leave-one-out testing')
+    loo_test_parser.description = 'Test the performance of a model using ' \
+        'leave-one-out testing'
+    loo_test_parser.add_argument('model', help='The JSON file containing the '
+        'model created by build-model')
+
+    args = command_parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
